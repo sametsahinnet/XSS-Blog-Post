f8fe57c160a2490ce603cfd848a10faab95d8bab
Samet SAHIN <41492787+sametsahinnet@users.noreply.github.com>
2019-06-27 17:48:26 +0300

    Add files via upload

---
+++ XSS-with-CSRF.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Steal CSRF Tokens with Simple XSS </title>
+</head>
+<body>
+	<h4>Steal CSRF Tokens with Simple XSS </h4>
+	<h6>by @sametsahinnet</h6>
+
+
+
+	<?php 
+	$benCSRFtoken = "samet";
+	$printMD5 = md5($benCSRFtoken);
+	?>
+
+
+	<a href="?search=query">XSS with CSRF ! </a>
+
+	<form>
+
+		<input type="hidden" name="search" value="<?php echo $_GET['search']; ?>">	
+
+		<input type="hidden" name="CSRFtoken" value="<?php echo $printMD5 ?>">
+
+		<input type="submit" value="Submit">
+	
+	</form>
+
+
+</body>
+</html>
---
+++ normal-XSS.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Simple XSS </title>
+</head>
+<body>
+	<h4>Simple XSS </h4>
+	<h6>by @sametsahinnet</h6>
+
+
+
+	<?php 
+	$benCSRFtoken = "samet";
+	$printMD5 = md5($benCSRFtoken);
+	?>
+
+
+
+	<!--<a href="?search=query">Only XSS ! </a>-->
+
+	<form>
+
+		<input type="hidden" name="CSRFtoken" value="<?php echo $printMD5 ?>">
+
+		<input type="text" name="search" value="<?php echo $_GET['search']; ?>">	
+
+		<input type="submit" value="Submit">
+
+	</form>	
+
+
+
+</body>
+</html>
